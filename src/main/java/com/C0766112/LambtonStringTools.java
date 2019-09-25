3695b150a42bb6e57e49cd3140ba0171fbf05e4c
CHARAN <charanpreetkaur026@gmail.com>
2019-09-25 19:15:43 -0400

    Reverse String

---
+++ src/main/java/com/C0766112/LambtonStringTools.java
@@ -0,0 +1,24 @@
+package com.C0766112;
+
+import java.util.Arrays;
+
+public class LambtonStringTools
+{
+
+
+    public static void main(String[] args)
+    {
+        TestStringTools object = new TestStringTools();
+        System.out.println(" Given String is: Lambton\n Reversed String is: " + object.reverse("Lambton"));
+       // String gString= "LAMBTON";
+        //String reverseString = get.reverseString;
+      //  reverseString r= String;
+        //System.out.println(" Reversed String is: " + reverseString);
+
+        //String binary = "1001";
+       // int decimal=
+    }
+
+
+
+}
---
+++ src/main/java/com/C0766112/TestStringTools.java
@@ -0,0 +1,36 @@
+package com.C0766112;
+
+import java.util.Arrays;
+
+public class TestStringTools {
+
+    private String gString;
+
+    public String reverse(String s)
+    {
+        StringBuilder revString = new StringBuilder();
+        for (int i = s.length() - 1; i >= 0; i--) {
+            revString.append(s.charAt(i));
+        }
+        return revString.toString();
+    }
+    /*
+    public int binaryToDecimal(String binary) {
+        int decimal = 0;
+        int binaryMultiplier = 1;
+        for (char c : reverse(binary).toCharArray()) {
+            if (c == '1') {
+                decimal += binaryMultiplier;
+            } else if (c == '0') {
+            } else {
+                return -1;
+            }
+            binaryMultiplier *= 2;
+        }
+        return decimal;
+    }
+    public String initaials()
+    {
+        String givenName=""
+    }*/
+}
